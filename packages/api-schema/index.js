46c27f4a6eb182847e52b550242d1b61ac69e5fe
Alex Dennis <alex.dennis@gmail.com>
2023-06-04 13:02:29 -0700

    prettier and main package.json updates

--- packages/api-schema/index.js
+++ packages/api-schema/index.js
@@ -1,13 +1,12 @@
 module.exports = {
-    validate: require("./lib/validate"),
-    schema: {
-      get idl() {
-        const join = require("path").join;
-        return require("fs").readFileSync(
-          join(__dirname, "schema.graphql"),
-          "utf8"
-        );
-      },
+  validate: require("./lib/validate"),
+  schema: {
+    get idl() {
+      const join = require("path").join;
+      return require("fs").readFileSync(
+        join(__dirname, "schema.graphql"),
+        "utf8"
+      );
     },
-  };
-  
+  },
+};
