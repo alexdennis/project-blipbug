b5625970ffd01921fd7c3e584f796bc4ca21ea61
Alex Dennis <alex.dennis@gmail.com>
2023-06-04 07:40:45 -0700

    Add schema

---
+++ packages/api-schema/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    validate: require("./lib/validate"),
+    schema: {
+      get idl() {
+        const join = require("path").join;
+        return require("fs").readFileSync(
+          join(__dirname, "schema.graphql"),
+          "utf8"
+        );
+      },
+    },
+  };
+  
---
+++ packages/api-schema/test/validate.js
@@ -0,0 +1,29 @@
+const assert = require("assert");
+
+const validate = require("../").validate;
+
+const goodQuery = validate(`
+{
+  ask(book: "The Bible", character: "Jesus", question: "What is the meaning of life?") {
+    answer
+  }
+}
+`);
+const badQuery = validate(`
+{
+  ask {
+    answer
+  }
+}
+`);
+
+assert.strict.equal(
+  goodQuery[0],
+  undefined,
+  "goodQuery validation returns no errors"
+);
+assert.match(
+  badQuery[0].message,
+  /Field "ask" argument "book" of type "String!" is required, but it was not provided./,
+  "badQuery validation returns GraphQLError error"
+);
