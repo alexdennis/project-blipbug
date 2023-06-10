442a072a7601c1518aeaf2785ab2360ec66e5688
Alex Dennis <alex.dennis@gmail.com>
2023-06-10 08:34:02 -0700

    Seed UI package

--- packages/infra/lib/infra-stack.ts
+++ packages/infra/lib/infra-stack.ts
@@ -1,12 +1,16 @@
-import { Stack, StackProps, Duration } from "aws-cdk-lib";
+import { Stack, StackProps, Duration, RemovalPolicy } from "aws-cdk-lib";
+import { Construct } from "constructs";
 import * as apigw from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import * as path from "path";
 
 export class InfraStack extends Stack {
   public readonly api: apigw.RestApi;
   public readonly apiLambda: lambda.Function;
+  public readonly websiteBucket: s3.Bucket;
+  public readonly websiteBucketDeployment: s3deploy.BucketDeployment;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -23,5 +27,30 @@ export class InfraStack extends Stack {
       handler: this.apiLambda,
       proxy: true,
     });
+
+    this.websiteBucket = new s3.Bucket(this, "ui", {
+      publicReadAccess: true,
+      blockPublicAccess: new s3.BlockPublicAccess({
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        blockPublicPolicy: false,
+        restrictPublicBuckets: false,
+      }),
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+    });
+
+    this.websiteBucketDeployment = new s3deploy.BucketDeployment(
+      this,
+      "ui-deployment",
+      {
+        sources: [
+          s3deploy.Source.asset(
+            path.join(__dirname, "..", "..", "ui", "build")
+          ),
+        ],
+        destinationBucket: this.websiteBucket,
+      }
+    );
   }
 }
--- packages/infra/test/infra.test.ts
+++ packages/infra/test/infra.test.ts
@@ -2,7 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import * as Infra from "../lib/infra-stack";
 
-test("SQS Queue and SNS Topic Created", () => {
+test("Functions created", () => {
   const app = new cdk.App();
   // WHEN
   const stack = new Infra.InfraStack(app, "MyTestStack");
